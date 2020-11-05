ed0e234ad3b3f303aff59bbfa50eec42c28dfb35
Eliana Souza <elianasouzarr@gmail.com>
2020-11-05 11:46:15 -0300

    php

--- fseletro.sql
+++ fseletro.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 26-Out-2020 às 22:30
+-- Tempo de geração: 05-Nov-2020 às 15:38
 -- Versão do servidor: 10.4.14-MariaDB
 -- versão do PHP: 7.4.11
 
@@ -20,6 +20,28 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `fseletro`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `comentarios`
+--
+
+CREATE TABLE `comentarios` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) DEFAULT NULL,
+  `msg` varchar(300) DEFAULT NULL,
+  `data` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `comentarios`
+--
+
+INSERT INTO `comentarios` (`id`, `nome`, `msg`, `data`) VALUES
+(1, 'diogo e fernanda', 'jjjjjj', '2020-10-28 21:17:53'),
+(2, '', '', '2020-11-03 16:17:18'),
+(3, 'maria', 'kkkkkkkkkkkkkkkkkkkk', '2020-11-03 16:17:34');
 
 -- --------------------------------------------------------
 
@@ -66,14 +88,20 @@ INSERT INTO `produtos` (`idprodutos`, `categoria`, `descricao`, `imagem`, `preco
 (6, 'microondas', 'Forno de Microondas Eletrolux 20L Branco', 'img/img/Forno de Microondas Eletrolux 20L Branco.jpg', '459.00', '436.00'),
 (7, 'microondas', 'Micro-ondas Consul 32 Litros Inox 220v.jpg', 'img/img/Micro-ondas Consul 32 Litros Inox 220v.jpg', '589.00', '409.00'),
 (8, 'microondas', 'Microondas 25L Espelhado Philco 220v', 'img/img/Microondas 25L Espelhado Philco 220v.jpg', '508.00', '464.00'),
-(9, 'lava louca', 'Lava Louça Compacta 8 Serviços Branca 127V Brastem', 'img/img/Lava Louça Compacta 8 Serviços Branca 127V Brastemp.jpg', '1970.00', '1730.00'),
-(10, 'lava louca', 'Lava-Louças Electrolux Inox com 10 Serviços, 06 Pr', 'img/img/Lava-Louças Electrolux Inox com 10 Serviços, 06 Programas de Lavagem e Painel Blue Touch.jpg', '3599.00', '2799.00'),
 (11, 'lavadora', 'Lavadora de Roupas Brastemp 11 kg com Turbo Perfor', 'img/img/Lavadora de Roupas Brastemp 11 kg com Turbo Performance Branca.jpg', '1699.00', '1214.00'),
-(12, 'lavadora', 'Lavadora de Roupas Philco Inverter 12KG', 'img/img/Lavadora de Roupas Philco Inverter 12KG.jpg', '2399.00', '2179.00');
+(12, 'lavadora', 'Lavadora de Roupas Philco Inverter 12KG', 'img/img/Lavadora de Roupas Philco Inverter 12KG.jpg', '2399.00', '2179.00'),
+(13, 'lavalouca', 'Lava-Louças Electrolux Inox com 10 Serviços, 06 Pr', 'img/img/Lava-Louças Electrolux Inox com 10 Serviços, 06 Programas de Lavagem e Painel Blue Touch.jpg', '3599.00', '2799.00'),
+(14, 'lavalouca', 'Lava Louça Compacta 8 Serviços Branca 127V Brastem', 'img/img/Lava Louça Compacta 8 Serviços Branca 127V Brastemp.jpg', '1970.00', '1730.00');
 
 --
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices para tabela `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Índices para tabela `pedidos`
@@ -92,6 +120,12 @@ ALTER TABLE `produtos`
 --
 
 --
+-- AUTO_INCREMENT de tabela `comentarios`
+--
+ALTER TABLE `comentarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT de tabela `pedidos`
 --
 ALTER TABLE `pedidos`
@@ -101,7 +135,7 @@ ALTER TABLE `pedidos`
 -- AUTO_INCREMENT de tabela `produtos`
 --
 ALTER TABLE `produtos`
-  MODIFY `idprodutos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `idprodutos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
